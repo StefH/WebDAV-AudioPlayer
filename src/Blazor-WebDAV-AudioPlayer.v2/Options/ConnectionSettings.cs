7af0a0ace65cfc657f1eb6c9403218588cb9f11d
Stef Heyenrath <stef.heyenrath@gmail.com>
2019-12-16 06:27:56 +0000

    .

--- src/Blazor-WebDAV-AudioPlayer.v2/Options/ConnectionSettings.cs
+++ src/Blazor-WebDAV-AudioPlayer.v2/Options/ConnectionSettings.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using WebDav.AudioPlayer.Client;
 using WebDav.AudioPlayer.IO;
 
@@ -7,13 +6,6 @@ namespace Blazor.WebDAV.AudioPlayer.Options
 {
     public class ConnectionSettings : IConnectionSettings
     {
-        //private readonly IOptions<ConnectionSettings> _options;
-
-        //public ConnectionSettings(IOptions<ConnectionSettings> options)
-        //{
-        //    _options = options;
-        //}
-
         public Uri StorageUri { get; set; }
 
         public string UserName { get; set; }
