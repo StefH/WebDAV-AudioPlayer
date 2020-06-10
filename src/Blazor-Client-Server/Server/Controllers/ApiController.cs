172ec9ddf41433ba362a2d9382275ca87799b3ed
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-06-10 10:41:29 +0000

    .

---
+++ src/Blazor-Client-Server/Server/Controllers/ApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Blazor_Client_Server.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> logger;
+
+        public ApiController(ILogger<ApiController> logger, IConnectionSettings connectionSettings)
+        {
+            this.logger = logger;
+        }
+
+       // [HttpGet]
+        
+    }
+}
